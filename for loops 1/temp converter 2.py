91a81f51304ebf774e3df99d3a3f63f71c07e371
Timothy Han <tth72189@gmail.com>
2016-04-20 15:21:21 -0400

    loops practice exercise

---
+++ for loops 1/temp converter 2.py
@@ -0,0 +1,16 @@
+def main():
+    x=GrabTemp()
+    TempConvert(x)
+
+def GrabTemp():
+    return float(input("Please Enter Temperature!(Fahrenheit Please!!)"))
+
+def TempConvert(x):
+    print("The Temperature in Celsius is",((x-32)*(5/9)),"Degrees" "! Also, the Temperature in Fahrenheit is",x,"Degrees!")
+    decision=str(input("Would you like to enter another temperature?(Y/N)"))
+    while decision>="y":
+        main()
+    else:
+        print("Thanks for using my program!")
+
+main()
