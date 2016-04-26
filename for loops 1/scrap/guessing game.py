286273028b09ca33ed4b0c0e69454e1f31a3ab1d
Timothy Han <tth72189@gmail.com>
2016-04-26 11:48:12 -0400

    added scrap/notes file

--- for loops 1/scrap/guessing game.py
+++ for loops 1/scrap/guessing game.py
@@ -13,13 +13,13 @@ def Compare():
         if Answer==Guess:
             print("Lucky Guess!")
             ValidGuess=True
+        elif b<=0:
+            print("Sorry bruh. Game Over")
+            ValidGuess=True
         elif Guess==Answer+1 or Guess==Answer-1:
             print("You're steaming hot!")
         elif Guess==Answer+2 or Guess==Answer-2:
             print("You're getting warmer..")
-        elif b<=0:
-            print("Sorry bruh. Game Over")
-            ValidGuess=True
         else:
             print("You're cold-blooded, bro")
         b-=1
